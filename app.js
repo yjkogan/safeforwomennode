458da82fe8c51187082c0df999960d0eb8e5ad2f
Yonatan Kogan <yjkogan@gmail.com>
2014-03-29 16:57:18 -0700

    Table name

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var express = require('express')
   , pg = require('pg');
 
 pg.connect(process.env.DATABASE_URL, function(err, client) {
-  var query = client.query('SELECT * FROM your_table');
+  var query = client.query('SELECT * FROM mentees');
 
   query.on('row', function(row) {
     console.log(JSON.stringify(row));
