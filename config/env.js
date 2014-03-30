4398b178c4a7a8c77f13ae8720cb4576ba56b380
Michael Wood <michael.wood@optimizely.com>
2014-03-30 15:46:13 -0700

    reset

--- config/env.js
+++ config/env.js
@@ -12,8 +12,8 @@ function set(name, value) {
 set('NODE_ENV', 'development');
 
 // The port for the HTTP server to listen on
-set('PORT', 5432);
-set('DATABASE_URL', 'postgres://ikpbwvrjkkccma@ec2-54-225-101-64.compute-1.amazonaws.com/dd5n435ihadob4');
+set('PORT', 8000);
+set('DATABASE_URL', 'postgres://michaelwood@localhost/sfw');
 
 // Twilio Info
 if (process.env.NODE_ENV == 'development') {
