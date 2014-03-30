b6e3ccdcc94b4ce75bfee883881bf7d7a95829bb
Yonatan Kogan <yjkogan@gmail.com>
2014-03-29 21:47:01 -0700

    Javascript to add a mentor

---
+++ public/js/add_mentor.js
@@ -0,0 +1,17 @@
+$(function() {
+  $("#add-mentor-btn").click(function(e) {
+    e.preventDefault();
+    var phone = $(this).siblings('.phone').val();
+    var mentee_id = $(this).siblings('.mentee_id').val();
+    $.ajax({
+      type: 'POST',
+      url: '/mentees',
+      data: {
+        phone: phone,
+        mentee_id: mentee_id
+      }
+    }).done(function(data) {
+      console.log(data);
+    });
+  })
+})
