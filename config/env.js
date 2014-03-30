0146b23b4ff606374b61f45ccac550c0143f027d
Yonatan Kogan <yjkogan@gmail.com>
2014-03-30 09:22:22 -0700

    Set up creds to work on heroku

--- config/env.js
+++ config/env.js
@@ -1,3 +1,4 @@
+var yaml = require('yamljs');
 function set(name, value) {
   if (!(name in process.env)) {
     process.env[name] = value;
@@ -13,3 +14,12 @@ set('NODE_ENV', 'development');
 // The port for the HTTP server to listen on
 set('PORT', 8000);
 set('DATABASE_URL', 'postgres://yjkogan@localhost/yjkogan');
+
+// Twilio Info
+if (process.env.NODE_ENV == 'development') {
+  var creds = yaml.load('./creds.yml');
+  set('ACCOUNT_SID', creds['account_sid']);
+  set('AUTH_TOKEN', creds['auth_token']);
+  set('TWILIO_NUMBER', creds['twilio_number']);
+  set('MY_NUMBER', creds['my_number']);
+}
